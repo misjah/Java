43f776fd24aa10b4fb1c1a9eb319a04c1d743a47
Milos Stojiljkovic <misjah@gmail.com>
2022-05-13 20:31:00 +0200

    Predavanja 13.05 Git Zadatak 1

---
+++ osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
@@ -0,0 +1,13 @@
+package p13_05_2022;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("git clone");
+        System.out.println("git status");
+        System.out.println("git add");
+        System.out.println("git commit");
+        System.out.println("git push");
+        System.out.println("touch - za kreiranje fajlova");
+        System.out.println("git restore");
+    }
+}
