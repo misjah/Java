743dbb15a4d15df9160f0819b5768943e15869bf
Milos Stojiljkovic <misjah@gmail.com>
2022-05-17 00:55:43 +0200

    Zadatak1 java

---
+++ osnove_jave/src/main/java/d16_05_2022/zadatak1.java
@@ -0,0 +1,22 @@
+package d16_05_2022;
+
+import java.util.Scanner;
+
+//1.	Napisati metodu stampajVrednostZa10Vecu u kojoj se stampa prosledjena vrednost za 10 veca.
+// Iz main-a pozvati izvrsenje metode za razlicite vrednosti.
+public class zadatak1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Unesi broj: ");
+        int broj = sc.nextInt();
+        stampajVrednostZa10Vecu(broj);
+
+    }
+
+    public static void stampajVrednostZa10Vecu(int numa) {
+        int proiz = numa * 10;
+        System.out.println("rezultat je: "+ proiz);
+
+    }
+}
+
