a5b44932a69cb1b93aa107ace302ea740a530116
Milos Stojiljkovic <misjah@gmail.com>
2022-05-17 12:41:04 +0200

    lapsus za * umesto +

---
+++ osnove_jave/src/main/java/d16_05_2022/Zadatak1_1.java
@@ -0,0 +1,25 @@
+package d16_05_2022;
+
+import java.util.Scanner;
+
+import static d16_05_2022.zadatak1.stampajVrednostZa10Vecu;
+
+public class Zadatak1_1 {
+    public static void main(String[] args) {
+        //1.	Napisati metodu stampajVrednostZa10Vecu u kojoj se stampa prosledjena vrednost za 10 veca.
+// Iz main-a pozvati izvrsenje metode za razlicite vrednosti.
+
+
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Unesi broj: ");
+                int broj = sc.nextInt();
+                stampajVrednostZa10Vecu(broj);
+
+            }
+
+            public static void stampajVrednostZa10Vecu(int uvecan) {
+                int proiz = uvecan + 10;
+                System.out.println("rezultat je: "+ proiz);
+                System.out.println("rezultat je: " + (uvecan + 10)); //drugaciji nacin
+    }
+}
